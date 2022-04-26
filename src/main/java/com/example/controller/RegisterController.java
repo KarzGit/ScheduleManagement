ab9855203cb7c36bd47a41ac6698c134b432381b
KarzGit <k.aso.utk0310@gmail.com>
2022-04-26 16:42:01 +0900

    微修正

--- src/main/java/com/example/controller/RegisterController.java
+++ src/main/java/com/example/controller/RegisterController.java
@@ -1,17 +1,12 @@
 package com.example.controller;
 
-import java.sql.Date;
-import java.sql.Time;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 
 import com.example.domain.User;
 import com.example.form.RegisterForm;
